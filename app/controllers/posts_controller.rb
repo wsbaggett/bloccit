f7aff5419e651091d045238048c7c85aeb332121
Walter Baggett <walterbaggett@legalshieldcorp.com>
2017-02-09 11:55:41 -0600

    assignment 18 complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,12 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+
+    @posts.each_with_index do |post, ind|
+      if ind % 5 == 0
+        post.update!(title: 'SPAM')
+      end
+    end
   end
 
   def show
