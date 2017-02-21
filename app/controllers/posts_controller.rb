2ee780c68582f37973eb46d40cfd19a96de1e227
Walter Baggett <walterbaggett@legalshieldcorp.com>
2017-02-21 11:14:41 -0600

    checkpoint 22 complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,10 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-  end
-
   def show
     @post = Post.find(params[:id])
   end
 
   def new
+    @topic = Topic.find(params[:topic_id])
     @post = Post.new
   end
 
@@ -16,12 +13,14 @@ class PostsController < ApplicationController
      @post = Post.new
      @post.title = params[:post][:title]
      @post.body = params[:post][:body]
+     @topic = Topic.find(params[:topic_id])
+     @post.topic = @topic
 
  # #10
      if @post.save
  # #11
        flash[:notice] = "Post was saved."
-       redirect_to @post
+       redirect_to [@topic, @post]
      else
  # #12
        flash.now[:alert] = "There was an error saving the post. Please try again."
@@ -40,7 +39,7 @@ class PostsController < ApplicationController
 
      if @post.save
        flash[:notice] = "Post was updated."
-       redirect_to @post
+       redirect_to [@post.topic, @post]
      else
        flash.now[:alert] = "There was an error saving the post. Please try again."
        render :edit
@@ -53,7 +52,7 @@ class PostsController < ApplicationController
  # #8
      if @post.destroy
        flash[:notice] = "\"#{@post.title}\" was deleted successfully."
-       redirect_to posts_path
+       redirect_to @post.topic
      else
        flash.now[:alert] = "There was an error deleting the post."
        render :show
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,20 @@
 require 'random_data'
 
+# Create Topics
+ 15.times do
+   Topic.create!(
+     name:         RandomData.random_sentence,
+     description:  RandomData.random_paragraph
+   )
+ end
+ topics = Topic.all
+
  # Create Posts
  50.times do
  # #1
    Post.create!(
  # #2
+     topic:  topics.sample,
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
    )
@@ -22,5 +32,6 @@ require 'random_data'
  end
 
  puts "Seed finished"
+ puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
