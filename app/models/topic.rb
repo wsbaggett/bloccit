c648848c8ebb445424882415f18153f96745cc1f
Walter Baggett <walterbaggett@legalshieldcorp.com>
2017-02-23 13:20:37 -0600

    assignment 23 complete

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,8 @@
 class Topic < ApplicationRecord
   has_many :posts, dependent: :destroy
+
+  validates :name, length: { minimum: 5 }, presence: true
+  validates :description, length: { minimum: 15 }, presence: true
+  validates :public, presence: true
+
 end
