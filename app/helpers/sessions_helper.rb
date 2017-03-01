3419a0f49404f04476f6900ded6391dec1c28e14
Walter Baggett <walterbaggett@legalshieldcorp.com>
2017-03-01 15:14:02 -0600

    assignment 26 complete

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,11 @@ module SessionsHelper
      session[:user_id] = user.id
    end
 
+   def avatar_url(user)
+     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+     "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+   end
+
  # #5
    def destroy_session(user)
      session[:user_id] = nil
