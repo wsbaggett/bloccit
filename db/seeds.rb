23b0612e30e2e8f9963859a6e6435585ad6d201e
Walter Baggett <walterbaggett@legalshieldcorp.com>
2017-03-08 13:04:59 -0600

    checkpoint 30 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,13 +22,16 @@ require 'random_data'
  # Create Posts
  50.times do
  # #1
-   Post.create!(
+   post = Post.create!(
  # #2
      user:   users.sample,
      topic:  topics.sample,
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
    )
+   post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+ # #13
+   rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
  end
  posts = Post.all
 
@@ -63,3 +66,4 @@ require 'random_data'
  puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
+ puts "#{Vote.count} votes created"
